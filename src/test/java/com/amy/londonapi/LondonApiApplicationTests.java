2d64590cc76f086f582eb73cf8f9fd9974e69d35
Amy Hetherington <amy.hetherington@engineering.digital.dwp.gov.uk>
2022-04-25 21:34:42 +0100

    initial project set up

---
+++ src/test/java/com/amy/londonapi/LondonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amy.londonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LondonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
