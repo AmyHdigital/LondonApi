55cfc780a841e69896f8eab82ad18136024c75d3
Amy Hetherington <amy.hetherington@engineering.digital.dwp.gov.uk>
2022-04-27 15:00:31 +0100

    feat(model): add model

---
+++ src/main/java/com/amy/londonapi/model/User.java
@@ -0,0 +1,28 @@
+package com.amy.londonapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class User {
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("ip_address")
+    private String ipAddress;
+
+    @JsonProperty("latitude")
+    private double latitude;
+
+    @JsonProperty("longitude")
+    private double longitude;
+}
